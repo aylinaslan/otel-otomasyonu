f3026f5b71ab57ebcb86aa0f3835ed68d93b2a7c
aylinaslan <98816021+aylinaslan@users.noreply.github.com>
2022-02-19 13:55:53 +0300

    next yöntemi
    Son tarihe kadar olan yıl bileşenini iletecek şekilde giriş denetleyicisinde Next yöntemini değiştirme.

---
+++ nextyönteminideğiştirme.cs
@@ -0,0 +1,29 @@
+public async Task<ActionResult> Next(SearchData model)
+    {
+        // Set the next page setting, and call the Index(model) action.
+        model.paging = "next";
+        await Index(model);
+
+        // Create an empty list.
+        var nextHotels = new List<string>();
+
+        // Add a hotel details to the list.
+        await foreach (var result in model.resultList.GetResultsAsync())
+        {
+            var ratingText = $"Rating: {result.Document.Rating}";
+            var rateText = $"Rates from ${result.Document.cheapest} to ${result.Document.expensive}";
+            var lastRenovatedText = $"Last renovated: {result.Document.LastRenovationDate.Value.Year}";
+
+            string fullDescription = result.Document.Description;
+
+            // Add strings to the list.
+            nextHotels.Add(result.Document.HotelName);
+            nextHotels.Add(ratingText);
+            nextHotels.Add(rateText);
+            nextHotels.Add(lastRenovatedText);
+            nextHotels.Add(fullDescription);
+        }
+
+        // Rather than return a view, return the list of data.
+        return new JsonResult(nextHotels);
+    }
